40b0f2a5314d8c3650f8c459b104bec3719a9579
Волков Вадим <volkovva.pir231@gamil.com>
2025-09-10 22:13:41 +0300

    feat: complete task 7

---
+++ Lab1/src/task7.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class task7 {
+    public static void main(String[] args) {
+        /*
+         * Разработать программу, которая позволит
+         *  при известном годовом проценте вычислить
+         * сумму вклада в банке через два года, если
+         * задана исходная величина вклада.  */
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите годовой процент: ");
+        float p = scanner.nextFloat();
+        if (p <= 0){
+            System.out.print("Введите годовой процент");
+            return;
+        }
+        System.out.print("Введите сумму вклада: ");
+        float v = scanner.nextFloat();
+        if (v <= 0){
+            System.out.print("Введите сумму вклада");
+            return;
+        }
+        float v1 = v+(v*p/100);// сумма вклада через год
+        float v2 = v1+(v1*p/100);// сумма вклада через 2 года
+        System.out.println("Сумма вклада через 2 года= " + v2);
+    }
+}
