6a37ff3fb9e21c2f41e0c0681b579a4136280633
Волков Вадим <volkovva.pir231@gamil.com>
2025-09-10 21:44:44 +0300

    feat: complete task 4

---
+++ Lab1/src/task4.java
@@ -0,0 +1,9 @@
+
+public class task4 {
+    public static void main(String[] args) {
+        /*
+         *   Написать программу, которая выведет на консоль «Hello world»,
+         не используя в исходном коде пробельных символов*/
+        System.out.println("Hello\u0020world"); /* юникод*/
+    }
+}
