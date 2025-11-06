039aa6a9eb6bc38e187574390b55da8fcc8614db
Волков Вадим <volkovva.pir231@gamil.com>
2025-11-06 09:48:52 +0300

    feat: complete lab13

--- src/main/java/com/example/servlets/UserServlet.java
+++ src/main/java/com/example/servlets/UserServlet.java
@@ -4,12 +4,13 @@ import com.example.User;
 import com.example.UserDAO;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
-
+@WebServlet
 public class UserServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
